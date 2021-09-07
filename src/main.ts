0ccd8fcb4197767ac8ee19595b07b4c5d2c6ee8b
Tushar Roy <binarydevelop@gmail.com>
2021-09-07 20:48:36 +0530

    add helmet module

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
 import { Logger } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 dotenv.config();
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(3000, () => {
     logger.log(`--- ${process.env.NODE_ENV} server started on ${process.env.PORT} ---`)
   })
